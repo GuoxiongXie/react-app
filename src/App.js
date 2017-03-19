00acf80dfc4f520d5c8b317f949320f1b7f0748a
Jake Xie <jake.xie@castar.com>
2017-03-19 16:20:40 -0700

    lesson 5: change state var

--- src/App.js
+++ src/App.js
@@ -3,17 +3,32 @@ import React from 'react';
 //const App = () => <h1>Hello</h1>;
 
 class App extends React.Component {
+    constructor() {
+        super();   // call 'super()' so that we can use 'this': Syntax error: 'this' is not allowed before super()
+        this.state = {
+            txt: "This is the state txt",
+            num: 0
+        };
+    }
+
+    update(event) {
+        this.setState({txt: event.target.value});
+    }
+
     render() {
         let text = this.props.txt, anotherText = this.props.anotherTxt, num = this.props.num;
 
         return (
             <div>
+                <input type="text" onChange={this.update.bind(this)} />
                 <h1>Hello Jake</h1>
                 <b>{text}</b>
                 <br />
                 <b>{anotherText}</b>
                 <br />
                 <b>Number is {num}</b>
+                <br />
+                <h2>{this.state.txt}</h2>
             </div>
         );
     }
