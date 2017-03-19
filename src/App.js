8433cdbf7e271f8d366b52132a572d55a85e43c3
Jake Xie <jake.xie@castar.com>
2017-03-19 14:46:31 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+//const App = () => <h1>Hello</h1>;
+
+class App extends React.Component {
+    render() {
+        return <h1>Hello Jake</h1>;
+    }
+}
+
+export default App;
